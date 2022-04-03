593cbb4ec7cf9c2db577db03a60dac2593f00d80
star <kingbar222@naver.com>
2022-04-03 15:46:14 +0900

    [수정] 그리드 기능 테스트

--- src/components/contents/Assignment/Form/General/AssignmentBudget.js
+++ src/components/contents/Assignment/Form/General/AssignmentBudget.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { Button, Container, Stack, TextField, Typography } from '@mui/material'
-import React, { useState, useRef, useCallback, useEffect } from 'react'
+import React, { useState, useRef, useCallback,useMemo, useEffect } from 'react'
 import { render } from 'react-dom'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/dist/styles/ag-grid.css'
@@ -9,6 +9,26 @@ import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css'
 
 const AssignmentBudget = () => {
   const gridRef = useRef ()
+
+  const abValueGetter = (params) => {
+    return (params.data.cash+ params.data.preCash+params.data.actual).parseInt;
+  };
+  const createData = (count, prefix) => {
+    let result = [];
+    for (let i = 0; i < count; i++) {
+      result.push({
+        detail3: '총 합계' ,
+        cash: prefix + ' Age ' + i,
+        preCash: prefix + ' Country ' + i,
+        actual: prefix + ' Year ' + i,
+        total:  '',
+      });
+    }
+    return result;
+  };
+  const pinnedBottomRowData = useMemo(() => {
+    return createData(1, 'Bottom');
+  }, []);
 
   const printResult = ( res ) => {
     console.log ( '---------------------------------------' )
@@ -37,20 +57,20 @@ const AssignmentBudget = () => {
   }
 
   const [rowData, setRowData] = useState ( [
-    { 비목: '직접비', 세목: '인건비', 세세목: '내부인건비', cash: '30000' },
-    { 비목: '직접비', 세목: '연구활동비', 세세목: '회의비', 예산금액: '30000' },
-    { 비목: '직접비', 세목: '연구활동비', 세세목: '출장비', 예산금액: '30000' },
+    { 비목: '직접비', 세목: '인건비', detail3: '내부인건비', cash: 30000 , preCash:2500, actual:11},
+    { 비목: '직접비', 세목: '연구활동비', detail3: '회의비', cash: 30000, preCash:2500, actual:11 },
+    { 비목: '직접비', 세목: '연구활동비', detail3: '출장비', cash: 30000, preCash:2500, actual:11 },
   ] )
 
   const addRow = () => {
     const newData = {
       비목: '직접비',
       세목: '연구활동비',
-      세세목: '장비구입',
-      cash: '0',
-      preCash: '0',
-      actual: '0',
-      total: '0',
+      detail3: '장비구입',
+      cash: 0,
+      preCash: 0,
+      actual: 0,
+      total: 0,
     }
     setRowData ( [...rowData, newData] )
     return rowData
@@ -69,13 +89,16 @@ const AssignmentBudget = () => {
   const GenderCellRenderer = ( props ) => {
     return props.value
   }
+
   const [columnDefs] = useState ( [
     {
       headerName: '사업비 항목',
       field: 'c1',
+      headerTooltip: '툴팁 내용',
       children: [
         {
           field: '비목',
+          headerTooltip: '툴팁 내용',
           cellEditor: 'agSelectCellEditor',
           cellEditorParams: {
             values: ['직접비', '간접비'],
@@ -89,7 +112,7 @@ const AssignmentBudget = () => {
           }
         },
         {
-          field: '세세목',
+          field: 'detail3',
           cellEditor: 'agSelectCellEditor',
           cellEditorParams: {
             values: ['내부인건비', '연구시설', 'others', 'others', 'others', 'others', 'others'],
@@ -100,13 +123,35 @@ const AssignmentBudget = () => {
       headerName: '예산금액',
       field: 'c2',
       children: [
-        { headerName: '현금', field: 'cash' },
+        { headerName: '현금', field: 'cash', },
         { headerName: '현금(이월분)', field: 'preCash' },
         { headerName: '현물', field: 'actual' },
-        { headerName: '계', field: 'total' },
+        { headerName: '계', field: 'total',
+          valueGetter: abValueGetter, }
       ],
     },
   ] )
+
+  const setPrinterFriendly = (api) => {
+    const eGridDiv = document.querySelector('#myGrid');
+    eGridDiv.style.height = '';
+    api.setDomLayout('print');
+  };
+
+  const setNormal = (api) => {
+    const eGridDiv = document.querySelector('#myGrid');
+    eGridDiv.style.width = '700px';
+    eGridDiv.style.height = '200px';
+    api.setDomLayout();
+  };
+  const onBtPrint = useCallback(() => {
+    const api = gridRef.current.api;
+    setPrinterFriendly(api);
+    setTimeout(function () {
+      print();
+      setNormal(api);
+    }, 2000);
+  }, []);
 
   return (
       <>
@@ -119,7 +164,11 @@ const AssignmentBudget = () => {
         <Button variant="contained" onClick={ clearData }>
           전체 삭제
         </Button>
+        <Button variant="contained" onClick={ onBtPrint }>
+          인쇄하기
+        </Button>
         <div
+            id="myGrid"
             className="ag-theme-balham-dark"
             style={ { height: 400, width: '100%' } }>
           <AgGridReact
@@ -128,6 +177,8 @@ const AssignmentBudget = () => {
               rowData={ rowData }
               columnDefs={ columnDefs }
               rowSelection="multiple"
+              tooltipShowDelay={10}
+              pinnedBottomRowData={pinnedBottomRowData}
           />
         </div>
       </>
--- src/components/contents/Main/MainTop.js
+++ src/components/contents/Main/MainTop.js
@@ -1,37 +1,16 @@
-import {useState, useCallback, useEffect, useRef, useContext} from 'react';
-import Header from '../../layouts/Header';
-import Nav from '../../layouts/Nav';
+
 import {
-  Avatar,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Divider,
-  IconButton,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
   Stack,
   Typography,
 } from '@mui/material';
 import * as React from 'react';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import MainCard from './MainCard';
 import ViewBox from '../ViewBox/ViewBox';
 
 const MainTop = () => {
   return (
     <ViewBox>
       <Stack direction={'row'}>
-        <MainCard value={'미결함'} />
-        <MainCard value={'진행함'} />
-        <MainCard value={'완료함'} />
-        <MainCard value={'반려함'} />
-        <MainCard value={'취소함'} />
+        main
       </Stack>
     </ViewBox>
   );
